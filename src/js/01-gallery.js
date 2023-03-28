417cb5e2a005193454f8ce6c78eee9ea7946c88b
oleniuh <maxim.oleniukh@gmail.com>
2023-03-28 22:15:23 +0100

    style fix and attempt to deploy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+    })
+    .join('');
+}
+
+gallery.addEventListener('click', pictureClickHandler);
+
+const lightBoxGallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(1);
