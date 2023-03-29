8c8030b3e7d4781079202664a0f7cdaa30a746be
oleniuh <maxim.oleniukh@gmail.com>
2023-03-29 11:16:01 +0100

    task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,33 +3,26 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-const galleryMarkup = createMarkup(galleryItems);
-
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-
 function createMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-        <li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+       <li class="gallery__item">
+   <a class="gallery__link"
+        href="${original}">
+      <img class="gallery__image"
+      src="${preview}" 
+      alt="${description}"/>
+   </a>
 </li>
 `;
     })
     .join('');
 }
+const galleryMarkup = createMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-gallery.addEventListener('click', pictureClickHandler);
-
-const lightBoxGallery = new SimpleLightbox('.gallery a', {
+const ligthBoxGallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-console.log(1);
