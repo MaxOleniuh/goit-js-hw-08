4901eac57e983f362466d759726446963d7dd010
MaxOleniuh <120305133+MaxOleniuh@users.noreply.github.com>
2023-03-30 20:09:12 +0000

    Deploying to gh-pages from @ MaxOleniuh/goit-js-hw-08@40325cff994479c96c927b8de369e1d494acb6b7 🚀

--- 02-video.b60b16f5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));o.on("timeupdate",(function(e){console.log(e)}));
-//# sourceMappingURL=02-video.b60b16f5.js.map
