06478407e9aa6f354e19283a7d879fadf2fd304b
oleniuh <maxim.oleniukh@gmail.com>
2023-03-30 11:19:46 +0100

    video player set up

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+const onPlay = function (data) {
+  console.log(data);
+};
+
+player.on('timeupdate', onPlay);
